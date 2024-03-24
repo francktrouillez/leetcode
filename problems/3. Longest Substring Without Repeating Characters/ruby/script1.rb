36d12c8863e8388b8f339ba4a116d44b56f87c75
Franck Trouillez <francktrouillez99@gmail.com>
2024-03-24 01:44:49 +0100

    Add solution for problem 3: Longest Substring Without Repeating Characters in Ruby

---
+++ problems/3. Longest Substring Without Repeating Characters/ruby/script1.rb
@@ -0,0 +1,26 @@
+# Problem: 3. Longest Substring Without Repeating Characters
+#
+# Given a string s, find the length of the longest substring without repeating characters.
+#
+# The idea is to keep track of the start of the substring and the index of the last seen character.
+# If the current character has been seen before and the last seen index is greater than the start of the substring,
+# then we update the start of the substring to the index of the last seen character + 1, because it is a
+# repeating character and we want to start the substring after the last seen character.
+
+# @param {String} s
+# @return {Integer}
+def length_of_longest_substring(s)
+  # Handle edge cases
+  return 0 if s.empty? # If the string is empty, then the longest substring is 0.
+  return 1 if s.length == 1 # If the string has only one character, then the longest substring is 1.
+
+  start = 0
+  hash = {}
+
+  s.each_char.with_index.reduce(0) do |max, (char, index)|
+    start = hash[char] + 1 if hash[char] && hash[char] >= start
+
+    hash[char] = index
+    [max, index - start + 1].max
+  end
+end
